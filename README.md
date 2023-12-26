079e506de443b328b85374e0f5747f434d3cfa80
Vivek Ranjan <vivek.ranjaan@gmail.com>
2023-12-26 16:47:51 +0100

    changed speed to time interval and added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Conway's Game of Life
+
+### Access the site [here](https://vivekranjan94.github.io/react-game-of-life/)
+
+The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.  
+It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input.
+
+The rules for the new generation are -
+
+- Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+- Any live cell with two or three live neighbours lives on to the next generation.
+- Any live cell with more than three live neighbours dies, as if by overpopulation.
+- Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ for (let i = 0; i < SIZE; i++) {
 export default function App() {
   const [board, setBoard] = useState(INITIAL_BOARD)
   const [int, setInt] = useState()
-  const [speed, setSpeed] = useState(250)
+  const [speed, setSpeed] = useState(1 / 250)
   const speedRef = useRef()
 
   function handleStartStop() {
@@ -26,7 +26,7 @@ export default function App() {
     setInt(
       setInterval(() => {
         setBoard(updateBoard(board))
-      }, speed)
+      }, 1 / speed)
     )
   }
 
@@ -53,7 +53,7 @@ export default function App() {
     setInt(
       setInterval(() => {
         setBoard(updateBoard(board))
-      }, speed)
+      }, 1 / speed)
     )
   }
 
@@ -72,12 +72,12 @@ export default function App() {
   return (
     <>
       <div className='slider-cont'>
-        <label htmlFor='speed'>Speed: {speed}</label>
+        <label htmlFor='speed'>Time Interval: {speed}</label>
         <input
           type='range'
           value={speed}
-          min='50'
-          max='1000'
+          min='0.001'
+          max='0.02'
           onChange={handleSpeedChange}
           ref={speedRef}
         />
